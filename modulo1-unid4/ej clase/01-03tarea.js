0dff82fbfc25cc64aee85767a3489bb47a50871c
CAROLINA ARDAIZ <ardaiz.carolina1@gmail.com>
2026-08-27 15:09:27 -0300

    tarean js

---
+++ modulo1-unid4/ej clase/01-03tarea.js
@@ -0,0 +1,14 @@
+const producto = 'ipad';
+
+const precio = 1000;
+
+const disponible = true;
+
+console.log(`el producto ${producto} tiene un precio de ${precio} y su disponibilidad es: ${disponible}`)
+
+
+const productos = ['leche', 'pan', 'huevos', 'manzanas'];
+
+for (let i = 0; i < productos.length; i++) {
+  console.log(`¡No te olvides de comprar ${productos[i]}!`);
+}
